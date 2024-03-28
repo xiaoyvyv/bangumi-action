3743904521aea2e84e58550a5a1a75f209127338
why <wanghuaiyu@highlight.mobi>
2024-03-28 14:37:29 +0800

    [Feat] v1.1.13

--- tmpl/tmpl.js
+++ tmpl/tmpl.js
@@ -1,6 +1,6 @@
 module.exports = {
     temp: "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
-        "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"480\" height=\"825\" class=\"\">\n" +
+        "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"480\" height=\"950\" class=\"\">\n" +
         "    <defs>\n" +
         "        <style/>\n" +
         "    </defs>\n" +
